000f74dfbc354106732251beae2013d480677542
Rodolfo "Dorfo" N. Silva <rodolfo.dorfo@outlook.com>
2024-04-10 20:47:34 -0300

    sistema de finalizar batalha iniciado e funcionando

--- Assets/Scripts/State Machine/TurnBeginState.cs
+++ Assets/Scripts/State Machine/TurnBeginState.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class TurnBeginState : State
 {
+    PlayerUnit _playerUnit;
+
     public override IEnumerator Enter()
     {
-        machine.CurrentUnit = machine.Units.Dequeue();
-        machine.Units.Enqueue(machine.CurrentUnit);
+        machine.CurrentUnit = null;
+
+        while (machine.CurrentUnit == null && machine.Units.Count > 0)
+        {
+            machine.CurrentUnit = machine.Units.Dequeue();
+            if (machine.CurrentUnit.GetStatValue(StatTypes.HP) <= 0)
+            {
+                Debug.LogFormat("{0} tried to play, but was dead", machine.CurrentUnit);
+                machine.CurrentUnit = null;
+            }else{
+                machine.Units.Enqueue(machine.CurrentUnit);
+            }
+        }
+        if (_playerUnit == null)
+        {
+            _playerUnit = machine.CurrentUnit as PlayerUnit;
+        }
 
         yield return new WaitForSeconds(0.1f);
-        StartCoroutine(WaitThenChangeState<RecoveryState>());
+
+        if (machine.Units.Count == 1 || _playerUnit.GetStatValue(StatTypes.HP) <= 0)
+        {
+            StartCoroutine(WaitThenChangeState<EndBattleState>());
+        }
+        else
+        {
+            StartCoroutine(WaitThenChangeState<RecoveryState>());
+        }
     }
 }
