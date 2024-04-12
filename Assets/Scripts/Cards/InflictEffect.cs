65967eeb1478c306699057cccf9c18e5ce35eeff
Rodolfo "Dorfo" N. Silva <rodolfo.dorfo@outlook.com>
2024-04-12 13:22:48 -0300

    stacking funcionando

--- Assets/Scripts/Cards/InflictEffect.cs
+++ Assets/Scripts/Cards/InflictEffect.cs
@@ -6,15 +6,56 @@ public class InflictEffect : CardEffect
 {
    public StatusEffect StatusEffectPrefab;
 
+   public int AppliesXTimes;
+
    public override IEnumerator Apply(List<object> targets)
    {
       foreach (object o in targets)
       {
          Unit unit = o as Unit;
-         StatusEffect instantiated = Instantiate(StatusEffectPrefab, Vector3.zero, Quaternion.identity, unit.transform); 
-         instantiated.name = StatusEffectPrefab.name;
+         for(int i=0; i<AppliesXTimes; i++)
+         {
+            TryToApply(unit);
+         }
 
          yield return null;
       }
+   }
+
+   void TryToApply(Unit unit)
+   {
+      StatusEffect status = GetEffect(unit);
+      if(status == null)
+      {
+         status = CreateEffect(unit);
+      }else{
+         if(StatusEffectPrefab.StacksDuration)
+         {
+            status.Duration += StatusEffectPrefab.Duration;
+         }
+         if(StatusEffectPrefab.StacksIntensity)
+         {
+            status.Amount += StatusEffectPrefab.Amount;
+         }
+      }
+   }
+
+   StatusEffect GetEffect(Unit unit)
+   {
+      foreach(StatusEffect status in unit.GetComponentsInChildren<StatusEffect>())
+      {
+         if(status.name == StatusEffectPrefab.name)
+         {
+            return status;
+         }
+      }
+      return null;
+   }
+
+   StatusEffect CreateEffect(Unit unit)
+   {
+      StatusEffect instantiated = Instantiate(StatusEffectPrefab, Vector3.zero, Quaternion.identity, unit.transform);
+      instantiated.name = StatusEffectPrefab.name;
+      return instantiated;
    }
 }
